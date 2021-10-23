5f2dcbecfe39ceb9747f5e9e16662b36c9f8c537
Crawfordpatt <crawfordpatten2@gmail.com>
2021-10-22 23:04:22 -0700

    copilot test

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// console.log("I'm Kanye West!");
-
-// function main() {
-//     x = prompt("Whats Your Name: ");
-//     document.getElementById("creepyText").innerHTML = "I can see you, " + x;
-// }
